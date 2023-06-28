61c8d25ce2b702e59ca3666bfb8b0d9985cc24f3
jbarajash <133033395+jbarajash@users.noreply.github.com>
2023-06-28 12:40:46 -0500

    Create wifiSTA.py

---
+++ Esquematicos/carpeta/wifiSTA.py
@@ -0,0 +1,11 @@
+import network
+sta_if = network.WLAN(network.STA_IF)
+
+def connectSTA(ssid, pwd):
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(ssid, pwd)
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
